0152f1cd61c68ad286a8b7cadaf027808cb9b38b
cduque <carduque@gmail.com>
2017-07-22 15:14:26 +0200

    counter people export

--- includes/counter_people_export.php
+++ includes/counter_people_export.php
@@ -4,41 +4,21 @@
 * Counter People viewer
 *
 */
-function DisplayCounterPeopleViewer(){
-  if( isset($_POST['counter_people']) ) {
-    if (CSRFValidate()) {
-		//error_log($_POST['time']);
-		$filename = "/opt/FeerBoxClient/feerbox-admin-web/pir.txt";
-		$handle = fopen($filename, "w");
-		fwrite($handle, date('Y-m-d H:i:s'));
-		fclose($handle);
-    }
-	else {
-    error_log('CSRF violation');
-  	}
-  }
+function DisplayCounterPeopleExport(){
+
  
   ?>
-   <script type="text/javascript">
-        $(document).ready(function(){
-            setInterval(function(){
-                $.get("includes/counterpeople_db.php<?php if(isset($_POST) && isset($_POST['counter_people'])){echo "?type=".$_POST['counter_type'];}?>", function(data){
-                    $("#pir").html(data);
-                });
-            }, 1000);
-        });
-    </script>
   <div class="row">
     <div class="col-lg-12">
       <div class="panel panel-primary">
-        <div class="panel-heading"><i class="fa fa-users fa-fw"></i> Counter People viewer</div>
+        <div class="panel-heading"><i class="fa fa-users fa-fw"></i> Counter People export</div>
         <!-- /.panel-heading -->
         <div class="panel-body">
           CounterPeople PIR: <div id="pir" style="font-size:xx-large;">0</div>
           <br/><br/><br/>
-          <form role="form" action="?page=counter_people" method="POST">
+          <form role="form" action="?page=counter_people_export" method="POST">
             <?php CSRFToken() ?>
-            <input type="hidden" name="counter_people" ?>
+            <input type="hidden" name="counter_people_export" ?>
 	        <!-- <input type="text" name="time" value="<?=date('Y-m-d H:i:s')?>">-->
 	        <select name="counter_type">
 	        	<option value="PIR" <?php if(isset($_POST) && isset($_POST['counter_people'])&& $_POST['counter_type']=="PIR"){echo "selected";}?>>PIR</option>
