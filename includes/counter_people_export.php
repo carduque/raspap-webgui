3bf30f3600e6764b2785ab21cdf0729e2f17dfa0
cduque <carduque@gmail.com>
2017-09-09 00:21:13 +0200

    Counter people export from first day of last month

--- includes/counter_people_export.php
+++ includes/counter_people_export.php
@@ -7,7 +7,8 @@
 function DisplayCounterPeopleExport(){
 	$db = new SQLite3('/opt/FeerBoxClient/FeerBoxClient/db/feerboxclient.db');
 	//Select min value from db
-	$query='SELECT min(time) as min_time FROM counterpeople where type="PIR"';
+	$month_ini = new DateTime("first day of last month");
+	$query='SELECT min(time) as min_time FROM counterpeople where type="PIR" and time>="'.date_format($month_ini,'Y-m-d H:i:s').'"';
 	$results = $db->query($query);
 	while ($row = $results->fetchArray()) {
 		$min_time = $row['min_time'];
@@ -35,7 +36,7 @@ function DisplayCounterPeopleExport(){
   <div class="row">
     <div class="col-lg-12">
       <div class="panel panel-primary">
-        <div class="panel-heading"><i class="fa fa-users fa-fw"></i> Counter People export</div>
+        <div class="panel-heading"><i class="fa fa-users fa-fw"></i> Counter People export - files from first day of last month</div>
         <!-- /.panel-heading -->
         <div class="panel-body">
           <br/><br/><br/>
