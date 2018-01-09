3fec88af05aa3d3701dcdf5a7dfb3ca26171fad9
cduque <carduque@gmail.com>
2018-01-09 22:27:41 +0100

    CounterPeople export

--- includes/counter_people_export.php
+++ includes/counter_people_export.php
@@ -41,7 +41,7 @@ function DisplayCounterPeopleExport(){
   <div class="row">
     <div class="col-lg-12">
       <div class="panel panel-primary">
-        <div class="panel-heading"><i class="fa fa-users fa-fw"></i> Counter People export - files from first day of last month</div>
+        <div class="panel-heading"><i class="fa fa-users fa-fw"></i> Counter People export - files from first day of 3 months ago</div>
         <!-- /.panel-heading -->
         <div class="panel-body">
           <br/><br/><br/>
